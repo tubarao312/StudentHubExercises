db981dbe41b480bf0537f0f0e1ddfe5474627e32
João Guilherme <joaommguilherme@tecnico.ulisboa.pt>
2023-11-02 18:00:14 +0000

    improved the magic exercise generator

--- scrape_exercises.py
+++ scrape_exercises.py
@@ -1,5 +1,19 @@
 import fitz  # PyMuPDF
 import re
+import os
+import string 
+import random
+from PIL import Image
+import numpy as np
+from json import dump
+
+s = string.ascii_lowercase + string.ascii_uppercase
+
+# Get the directory of the script
+script_directory = os.path.dirname(os.path.realpath(__file__))
+
+exercises_pdf_path = "sucessoes.pdf"
+solutions_pdf_path = "sucessoes_resol.pdf"
 
 def find_expression_positions(pdf_path, expression):
     with fitz.open(pdf_path) as doc:
@@ -60,32 +74,24 @@ def get_below_header_positions(pdf_path):
                 })
     return positions
 
-pdf_path = "test_pdf.pdf"
-expression = r"\b(?:Exame|Teste)\b"
+def get_positions(pdf_path):
+    expression = r"\b(?:Exame|Teste)\b"
+    positions_below_header = get_below_header_positions(pdf_path)
+    expression_positions = find_expression_positions(pdf_path, expression)
 
-positions_below_header = get_below_header_positions(pdf_path)
-expression_positions = find_expression_positions(pdf_path, expression)
+    """
+    Sort the positions by page number and y0 coordinate
+    TODO: First exercise wont be extracted, I need to find a way to get the upper margin of the first exercise (use the first appearence of 1.?)
+    """
 
-"""
-Sort the positions by page number and y0 coordinate
-TODO: First exercise wont be extracted, I need to find a way to get the upper margin of the first exercise (use the first appearence of 1.?)
-"""
+    positions = sorted(positions_below_header + expression_positions, key=lambda x: (x["page"], x["y0"]))
+    return positions
 
-positions = sorted(positions_below_header + expression_positions, key=lambda x: (x["page"], x["y0"]))
+def get_bounding_boxes(pdf_path):
+    positions = get_positions(pdf_path)
+    bboxes = []
 
-"""
-Extract an image of everything between the positions (up and down)
-Dont extract if there is a non header followed by a header
-"""
-
-from PIL import Image
-import numpy as np
-
-def extract_image(pdf_path, positions):
     with fitz.open(pdf_path) as doc:
-    
-        images = []
-        
         for i in range(len(positions) - 1):
             pos = positions[i]
             next_pos = positions[i + 1]
@@ -96,26 +102,110 @@ def extract_image(pdf_path, positions):
             # Skip if the positions are less than 20 pixels apart
             if abs(pos["y0"] - next_pos["y0"]) < 20:
                 continue
-            
+
             page = doc[pos["page"] - 1]
-            
-            # Extract the image between y0 and y1 of the positions, x0 and x1 are the entire page length
+
             x0, x1 = 0, int(page.bound().width)
             y0, y1 = int(min(pos["y1"], next_pos["y0"])), int(max(pos["y1"], next_pos["y0"]))
+
+            bboxes.append({
+                "x0": x0,
+                "y0": y0,
+                "x1": x1,
+                "y1": y1,
+                "page": pos["page"]
+            })
+
+    return bboxes
+
+def extract_images(pdf_path):
+    """
+    Extract an image of everything between the positions (up and down)
+    """
+
+    bboxes = get_bounding_boxes(pdf_path)
+    with fitz.open(pdf_path) as doc:
+        images = []
+        
+        for bbox in bboxes:
+            zoom = 3    # zoom factor not to lose the quality
+            mat = fitz.Matrix(zoom, zoom)
+            page = doc[bbox["page"] - 1]
+            pix = page.get_pixmap(matrix = mat)
             
-            pix = page.get_pixmap(alpha=False)
+            # Extract the image between y0 and y1 of the positions, x0 and x1 are the entire page length
+            x0, x1, y0, y1 = bbox["x0"], bbox["x1"], bbox["y0"], bbox["y1"]
+            
+            pix = page.get_pixmap(alpha=False, matrix=mat, clip=[x0, y0, x1, y1])
             img = Image.frombytes("RGB", [pix.width, pix.height], pix.samples)
             img = np.array(img)
 
-            img = img[y0:y1, x0:x1]
+            # img = img[y0:y1, x0:x1]
             images.append(img)
             
         return images
 
-images = extract_image(pdf_path, positions)
+def search_for_answer(regex, page, left, top, right, bottom):
+    r = fitz.Rect(left, top, right, bottom)  # Define the search region
+        
+    text = page.get_textpage(clip=r)
+    text = text.extractText()
 
-# Save images
-for i, image in enumerate(images):
-    print(image.shape)
-    image = Image.fromarray(image)
-    image.save(f"exercise_{i}.png")
+    matches = re.findall(regex, text)
+    if matches:
+        return matches[0][-1] # Return the last letter of the match (A, B, C or D)
+
+def get_answer(pdf_path):
+    bboxes = get_bounding_boxes(pdf_path)
+    answers = []
+    with fitz.open(pdf_path) as doc:
+        for bbox in bboxes:
+            page = doc[bbox["page"] - 1]
+            answers.append(search_for_answer(r"Resposta: .+ [ABCD]", page, bbox["x0"], bbox["y0"], bbox["x1"], bbox["y1"]))
+
+    answer_jsons = []
+    for answer in answers:
+        if not answer:
+            answer_jsons.append(None)
+            continue
+
+        l = ["A", "B", "C", "D"]
+        l.remove(answer)
+
+        opt2, opt3, opt4 = random.sample(l, 3)
+        answer_jsons.append({
+            "author": "Python",
+            "difficulty": "CHANGE THIS",
+            "answers" : [
+                answer,
+                opt2,
+                opt3,
+                opt4
+            ]
+        })
+
+    return answer_jsons
+
+exercises_images = map(Image.fromarray, extract_images(exercises_pdf_path))
+solutions_images = map(Image.fromarray, extract_images(solutions_pdf_path))
+answers = get_answer(solutions_pdf_path)
+
+for i, (ex, sol, ans) in enumerate(zip(exercises_images, solutions_images, answers)):
+    if not ans:
+        continue
+
+    rand_string = "".join(random.choices(s, k=10))
+    folder_name = f"ex_{rand_string}"
+    folder_path = os.path.join(script_directory, folder_name)
+
+    # Use os.makedirs() to create the folder
+    os.makedirs(folder_path, exist_ok=True)
+
+    # Save the images and the json in the created folder
+    ex.save(os.path.join(folder_path, "question.png"))
+    sol.save(os.path.join(folder_path, "tip1.png"))
+
+    with open(os.path.join(folder_path, "info.json"), "w") as f:
+        dump(ans, f, indent=4)
+
+    print(f"Created folder {folder_name}")    
